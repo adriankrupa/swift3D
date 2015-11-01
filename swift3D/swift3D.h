0ddea579b8058f21f4f72237f9f6361dd94f6c36
Adrian Krupa <adrian.krupa91@gmail.com>
2015-11-01 19:22:46 +0100

    Initial Commit

---
+++ swift3D/swift3D.h
@@ -0,0 +1,19 @@
+//
+//  swift3D.h
+//  swift3D
+//
+//  Created by Adrian Krupa on 01.11.2015.
+//  Copyright © 2015 Adrian Krupa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swift3D.
+FOUNDATION_EXPORT double swift3DVersionNumber;
+
+//! Project version string for swift3D.
+FOUNDATION_EXPORT const unsigned char swift3DVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift3D/PublicHeader.h>
+
+
---
+++ swift3DTests/swift3DTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift3DTests.swift
+//  swift3DTests
+//
+//  Created by Adrian Krupa on 01.11.2015.
+//  Copyright © 2015 Adrian Krupa. All rights reserved.
+//
+
+import XCTest
+@testable import swift3D
+
+class swift3DTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
